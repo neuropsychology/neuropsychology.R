6270df568287d347351d59d2307f129a5e132429
DominiqueMakowski <dom.mak19@gmail.com>
2016-08-26 15:21:59 +0200

    cortable

--- R/cortable.R
+++ R/cortable.R
@@ -1,37 +1,63 @@
-cortable <- function(x, correction="none", type="p"){
+cortable <- function(df, 
+                     correction="none", 
+                     type="pearson", 
+                     print.result=TRUE,
+                     plot.result=TRUE){
 
-  type <- ifelse(type == "s", "spearman", "pearson")
+  type <- ifelse(type == "s", "spearman",
+                 ifelse(type == "spearman", "spearman","pearson"))
 
-  for (i in names(x)){
-    if (is.numeric(x[,i]) == FALSE){
-      x[,i] = NULL
+
+  for (i in names(df)){
+    if (is.numeric(df[,i]) == FALSE){
+      df[,i] = NULL
     }
   }
-  x <- as.matrix(x)
+  dimnames <- names(df)
+  df <- as.matrix(df)
 
 
 
-  R <- rcorr(x, type = type)$r
-  p <- rcorr(x, type = type)$P
-  p <- p.adjust(p,method = correction)
+  R <- rcorr(df, type = type)$r
+  p <- rcorr(df, type = type)$P
+  p <- p.adjust(p, method = correction)
+  p.mat <- matrix(p, ncol = ncol(R), dimnames = list(dimnames,dimnames))
+
   ## define notions for significance levels; spacing is important.
   mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
 
   ## trunctuate the matrix that holds the correlations to two decimal
-  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
+  R <- format(round(cbind(rep(-1.11, ncol(df)), R), 2))[,-1]
 
   ## build a new matrix that includes the correlations with their apropriate stars
-  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
-  diag(Rnew) <- paste(diag(R), " ", sep="")
-  rownames(Rnew) <- colnames(x)
-  colnames(Rnew) <- paste(colnames(x), "", sep="")
+  table <- matrix(paste(R, mystars, sep=""), ncol=ncol(df))
+  diag(table) <- paste(diag(R), " ", sep="")
+  rownames(table) <- colnames(df)
+  colnames(table) <- paste(colnames(df), "", sep="")
 
   ## remove upper triangle
-  Rnew <- as.matrix(Rnew)
-  Rnew[upper.tri(Rnew, diag = TRUE)] <- NA
-  Rnew <- as.data.frame(Rnew)
+  table <- as.matrix(table)
+  table[upper.tri(table, diag = TRUE)] <- NA
+  table <- as.data.frame(table)
 
   ## remove last column and return the matrix (which is now a data frame)
-  Rnew <- cbind(Rnew[1:length(Rnew)-1])
-  return(Rnew)
+  table <- cbind(table[1:length(table)-1])
+  
+  if (print.result==TRUE){
+    print(table)
+  }
+  if (plot.result==TRUE){
+    print(ggcorrplot(rcorr(df, type = type)$r,
+               title = paste("A ", type, "'s correlation matrix (correction: ", correction, ")", sep = ""),
+               method = "circle",
+               type="lower",
+               colors=c("#E91E63", "white", "#03A9F4"),
+               hc.order = TRUE,
+               p.mat = p.mat,
+               insig="pch",
+               legend.title="",
+               lab = FALSE))
+  }
+  
+  return(as.data.frame(table))
 }
