7b46af52e1ea55cd3f8d1bde2501313dcec4cf8e
Dominique Makowski <dom.mak19@gmail.com>
2016-09-08 17:29:32 +0200

    Update cortable.R
    Return the plot

--- R/cortable.R
+++ R/cortable.R
@@ -3,13 +3,13 @@ cortable <- function(df,
                      type="pearson",
                      print.result=TRUE,
                      plot.result=TRUE){
-
+  
   type <- ifelse(type == "s", "spearman",
                  ifelse(type == "spearman", "spearman","pearson"))
-
+  
   correction_text <- ifelse(correction=="holm", "Holm-Bonferroni",
                             ifelse(correction=="fdr", "False Discovery Rate",correction))
-
+  
   for (i in names(df)){
     if (is.numeric(df[,i]) == FALSE){
       df[,i] = NULL
@@ -17,53 +17,53 @@ cortable <- function(df,
   }
   dimnames <- names(df)
   df <- as.matrix(df)
-
-
-
+  
+  
+  
   R <- rcorr(df, type = type)$r
   p <- rcorr(df, type = type)$P
   p <- p.adjust(p, method = correction)
   p.mat <- matrix(p, ncol = ncol(R), dimnames = list(dimnames,dimnames))
-
+  
   ## define notions for significance levels; spacing is important.
   mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
-
+  
   ## trunctuate the matrix that holds the correlations to two decimal
   R <- format(round(cbind(rep(-1.11, ncol(df)), R), 2))[,-1]
-
+  
   ## build a new matrix that includes the correlations with their apropriate stars
   table <- matrix(paste(R, mystars, sep=""), ncol=ncol(df))
   diag(table) <- paste(diag(R), " ", sep="")
   rownames(table) <- colnames(df)
   colnames(table) <- paste(colnames(df), "", sep="")
-
+  
   ## remove upper triangle
   table <- as.matrix(table)
   table[upper.tri(table, diag = TRUE)] <- NA
   table <- as.data.frame(table)
-
+  
   ## remove last column and return the matrix (which is now a data frame)
   table <- cbind(table[1:length(table)-1])
-
+  
   if (print.result==TRUE){
     print(paste("A ", type, "'s correlation matrix (correction: ", correction_text, ")", sep = ""))
     print(table)
   }
   
   plot <- ggcorrplot(rcorr(df, type = type)$r,
-               title = paste("A ", type, "'s correlation matrix (correction: ", correction_text, ")", sep = ""),
-               method = "circle",
-               type="lower",
-               colors=c("#E91E63", "white", "#03A9F4"),
-               hc.order = TRUE,
-               p.mat = p.mat,
-               insig="pch",
-               legend.title="",
-               lab = FALSE)
-               
+                     title = paste("A ", type, "'s correlation matrix (correction: ", correction_text, ")", sep = ""),
+                     method = "circle",
+                     type="lower",
+                     colors=c("#E91E63", "white", "#03A9F4"),
+                     hc.order = TRUE,
+                     p.mat = p.mat,
+                     insig="pch",
+                     legend.title="",
+                     lab = FALSE)
+  
   if (plot.result==TRUE){
     print(plot)
   }
-
-  return(c(as.data.frame(table), plot))
+  
+  return(list(df=as.data.frame(table), plot=plot))
 }
