5021aefbe79df65b3d13bd85f93dce1f84f04f4f
Dominique Makowski <dom.mak19@gmail.com>
2016-09-16 14:57:01 +0200

    Add files via upload

---
+++ R/APAze.R
@@ -0,0 +1,25 @@
+APAze <- function(fit, method="boot"){
+  confint <- confint(fit, method=method, nsim=1000,oldNames=F)
+  coefs <- data.frame(coef(summary(fit)))
+  coefs$CI25 <- tail(confint,nrow(coefs))[,1]
+  coefs$CI75 <- tail(confint,nrow(coefs))[,2]
+  p_list <- 2 * (1 - pnorm(abs(coefs$t.value)))
+  coefs <- subset(coefs,select=-c(Std..Error,t.value))
+  coefs <- round(coefs,2)
+  coefs$p <- round(p_list,3)
+
+  p <- ifelse(coefs$p < .001, "< .001",
+              ifelse(coefs$p < .01, "< .01",
+                     ifelse(coefs$p < .05, "< .05",
+                            paste("= ", coefs$p, sep=""))))
+  apa <- paste(
+    paste(rownames(coefs), ": ", sep=""),
+    paste("(ƒÀ = ", as.character(coefs$Estimate), ",", sep=""),
+    paste(" 95% CI [", as.character(coefs$CI25), ", ", sep=""),
+    paste("", as.character(coefs$CI75), "]", sep=""),
+    paste(", p ", p, ")", sep=""),
+    sep=""
+  )
+  return(apa)
+}
+            
