232c69b24d482eed25cd5035eabe23a5b906cdc2
DominiqueMakowski <dom.mak19@gmail.com>
2016-09-21 14:59:02 +0200

    0.1 CRAN

--- R/APAze.R
+++ R/APAze.R
@@ -1,13 +1,15 @@
-APAze <- function(fit, method="boot"){
+APAze <- function(fit, method="boot", nsim=1000){
 
+  varsnames <- all.vars(terms(fit))
   R2 <- r.squaredGLMM(fit)
 
-  confint <- confint(fit, method=method, nsim=1000,oldNames=F)
+  confint <- confint(fit, method=method, nsim=nsim,oldNames=F)
   coefs <- data.frame(coef(summary(fit)))
   coefs$CI25 <- tail(confint,nrow(coefs))[,1]
   coefs$CI75 <- tail(confint,nrow(coefs))[,2]
   p_list <- 2 * (1 - pnorm(abs(coefs$t.value)))
-  coefs <- subset(coefs,select=-c(Std..Error,t.value))
+  coefs <- coefs[,!(names(coefs) %in% c("Std..Error","t.value"))]
+
   coefs <- round(coefs,2)
   coefs$p <- round(p_list,3)
 
@@ -17,12 +19,14 @@ APAze <- function(fit, method="boot"){
                             ifelse(coefs$p >= 1.00, ">= 1.00",
                             paste("= ", substring(as.character(format(round(coefs$p, 2), nsmall=2)), 2), sep="")))))
 
-  R2_apa <- paste("The overall model predicting ... successfully converged and explained ",
+  R2_apa <- paste("The overall model predicting ",
+  varsnames[1],
+  " successfully converged and explained ",
   substring(as.character(format(round(R2[2], 2), nsmall=2)), 3),
-  "% of the variance of the endogen (the conditional R²). ",
+  "% of the variance of the endogen (the conditional R2). ",
   "The variance explained by the fixed effects was of ",
   substring(as.character(format(round(R2[1], 2), nsmall=2)), 3),
-  "% (the marginal R²) and the one explained by the random effects of ",
+  "% (the marginal R2) and the one explained by the random effects of ",
   substring(as.character(format(round(R2[2]-R2[1], 2), nsmall=2)), 3),
   "%.", sep="")
 
