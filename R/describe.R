da978a2057c14e1734929ce9a08db2e0b66059a5
DominiqueMakowski <dom.mak19@gmail.com>
2016-09-26 16:56:02 +0200

    0.1.3

--- R/describe.R
+++ R/describe.R
@@ -5,8 +5,9 @@ describe <- function(df, group=NA){
 
     n=1
     for (i in df.list){
+      i <- i[ , !(names(i) %in% group)]
       print(prettyR::describe(i, xname=levels(df[,group])[n]))
-      n=+1
+      n=n+1
       }
   } else{
     print(prettyR::describe(df, horizontal=FALSE))
