3a3b3df4ddcdcbfd0edbeffe7b348c36773271f5
DominiqueMakowski <dom.mak19@gmail.com>
2016-09-29 00:40:11 +0200

    plot2

--- tests/testthat/test-assess.R
+++ tests/testthat/test-assess.R
@@ -3,11 +3,12 @@ library("devtools")
 install_github("neuropsychology/neuropsychology.R", dependencies=TRUE)
 library(neuropsychology)
 
-assess(score=27, population=personality$Age)
 
 figure <- assess(87, mean=100, sd=15)
+figure2 <- assess(score=27, distribution=personality$Age, colour.fill="grey", colour.line="black")
 
 ggsave("assess_plot.png", figure, dpi=600,
        scale=1.2)
-
+ggsave("assess_plot2.png", figure2, dpi=600,
+       scale=1.2)
 # ggsave("mafigure.png", assess(87, mean=100, sd=15, language="fr", colour.line="black", colour.fill="gray"))
