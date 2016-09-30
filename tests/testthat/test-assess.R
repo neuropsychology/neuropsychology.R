546850484dbf32a23985f14b326079114cfee0f3
DominiqueMakowski <dom.mak19@gmail.com>
2016-10-01 01:03:28 +0200

    Example plots size

--- tests/testthat/test-assess.R
+++ tests/testthat/test-assess.R
@@ -8,7 +8,9 @@ figure <- assess(87, mean=100, sd=15)
 figure2 <- assess(score=27, distribution=personality$Age, colour.fill="grey", colour.line="black")
 
 ggsave("assess_plot.png", figure, dpi=600,
-       scale=1.2)
+       width=8, height=4.5)
 ggsave("assess_plot2.png", figure2, dpi=600,
-       scale=1.2)
+       width=8, height=4.5)
 # ggsave("mafigure.png", assess(87, mean=100, sd=15, language="fr", colour.line="black", colour.fill="gray"))
+library(ggplot2)
+?ggsave
