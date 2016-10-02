eaba57e73620030a5322dd29c21d031010076194
DominiqueMakowski <dom.mak19@gmail.com>
2016-10-03 01:11:49 +0200

    0.2.4

--- R/extract_text.R
+++ R/extract_text.R
@@ -3,16 +3,16 @@ extract_text <- function(files=".",
                           word.length.max=Inf,
                           freq.min=10,
                           freq.max=Inf){
-#library(tm)
-  Rpdf <- tm::readPDF(control = list(text = "-layout"))
+
+  Rpdf <- readPDF(control = list(text = "-layout"))
   if(grepl(".pdf",files)){
     files <- c(files)
   }
   else{
     files <- list.files(path=files, pattern = "pdf$")
   }
-  text <- tm::Corpus(URISource(files), readerControl = list(reader = Rpdf))
-  text_clean <- tm::TermDocumentMatrix(text, control = list(removePunctuation = TRUE,
+  text <- Corpus(URISource(files), readerControl = list(reader = Rpdf))
+  text_clean <- TermDocumentMatrix(text, control = list(removePunctuation = TRUE,
                                                        tolower = TRUE,
                                                        removeNumbers = TRUE,
                                                        stopwords = TRUE,
@@ -21,7 +21,7 @@ extract_text <- function(files=".",
                                                        # bounds = list(global = c(2, Inf))
   ))
   
-  words <- tm::findFreqTerms(text_clean, lowfreq=freq.min, highfreq=freq.max)
+  words <- findFreqTerms(text_clean, lowfreq=freq.min, highfreq=freq.max)
   words <- as.data.frame(inspect(text_clean[words,]))
   data <- data.frame(word=rownames(words),freq=rowSums(words))
   data <- arrange(data, desc(freq))
