6cd6c8b1a81217d4e5975595c061bb2fc34dca23
DominiqueMakowski <dom.mak19@gmail.com>
2016-10-04 01:08:48 +0200

    0.2.5

--- tests/testthat/test-cortable.R
+++ tests/testthat/test-cortable.R
@@ -1,12 +1,9 @@
 install.packages("devtools")
 library("devtools")
 install_github("neuropsychology/neuropsychology.R", dependencies=TRUE)
-
 library(neuropsychology)
 
-df <- personality
-
-cortable(df)
+table <- cortable(personality)
 
 ggsave("cortable_plot.png", cortable(df, returns="plot"), dpi=600,
        scale=1.2)
