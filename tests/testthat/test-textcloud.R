aaaac6268b77bda760c99b94de9ad0fc602c24aa
DominiqueMakowski <dom.mak19@gmail.com>
2016-10-03 02:20:56 +0200

    0.2.4

--- tests/testthat/test-textcloud.R
+++ tests/testthat/test-textcloud.R
@@ -4,4 +4,9 @@ install_github("neuropsychology/neuropsychology.R", dependencies=TRUE)
 library(neuropsychology)
 
 textcloud(image=NULL)
-textcloud(image="head2")
+
+
+png(filename="textcloud.png")
+plot(textcloud(image="head1", text.size = 0.8))
+dev.off()
+
