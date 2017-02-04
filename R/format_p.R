893265bba91ef9e1bbcb5ceb19a66d5c6fecee1a
DominiqueMakowski <dom.mak19@gmail.com>
2017-02-04 16:41:37 +0100

    format_p fix

--- R/format_p.R
+++ R/format_p.R
@@ -3,13 +3,13 @@ format_p <- function(p, add.stars=TRUE){
     p <- ifelse(p < .001, "< .001",
                 ifelse(p < .01, "< .01",
                        ifelse(p < .05, "< .05",
-                              ifelse(p >= 0.1, ">= .1",
+                              ifelse(p >= 0.1, "> .1",
                                      paste("= ", substring(as.character(format(round(p, 2), nsmall=2)), 2), "^", sep="")))))
   } else{
     p <- ifelse(p < .001, "< .001***",
                 ifelse(p < .01, "< .01**",
                        ifelse(p < .05, "< .05*",
-                              ifelse(p >= 0.1, ">= .1",
+                              ifelse(p >= 0.1, "> .1",
                                      paste("= ", substring(as.character(format(round(p, 2), nsmall=2)), 2), "^", sep="")))))
     
   }
