829b30e9387f615a9f4beafd3baef22b65e1bb15
DominiqueMakowski <dom.mak19@gmail.com>
2017-02-04 16:15:00 +0100

    added warning

--- R/ASQ_subjective_mood_disorder.R
+++ R/ASQ_subjective_mood_disorder.R
@@ -6,6 +6,7 @@ ASQ_subjective_mood_disorder <- function(Suppression, Pretending, Feeling_of_Con
                     "Cognitive_Change"=Cognitive_Change,
                     "Acceptance"=Acceptance)
   prob <- predict(neuropsychology::models$ASQ_tree, data, type="prob")[2]
+  message("Warning: The following result is based on the method described in Makowski et al. (2017), which presents severe limitations. Use for exploration only.")
   print(paste("The probability of subjectively reporting a mood disorder is", round(prob*100, 2), "%"))
   return(prob)
 }
