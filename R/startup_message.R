493d6f223746d3567887793573d55fb05f35c574
DominiqueMakowski <dom.mak19@gmail.com>
2017-02-05 17:26:54 +0100

    startup message

--- R/startup_message.R
+++ R/startup_message.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("************\nWelcome to neuropsychology v0.3.0 (c) Dominique Makowski.")
+  packageStartupMessage("************\nWelcome to neuropsychology v0.4.0 (c) Dominique Makowski.")
   packageStartupMessage("See documentation on https://www.rdocumentation.org/packages/neuropsychology")
   packageStartupMessage("Do not hesitate to create an issue on https://github.com/neuropsychology/neuropsychology.R/issues with questions, comments, or movie recommendations.\n************")
 }
