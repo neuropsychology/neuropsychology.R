ebed4d9d8f5ddf887c3f359c2c0a50839248b6f3
Dominique Makowski <dom.mak19@gmail.com>
2017-02-21 16:31:04 +0100

    dprime fixed for extreme values adjustment

--- R/dprime.R
+++ R/dprime.R
@@ -1,15 +1,22 @@
 dprime <- function(n_hit, n_miss, n_fa, n_cr){
+  # Ratios
+  hit_rate <- n_hit/(n_hit + n_miss)
+  fa_rate <- n_fa/(n_fa + n_cr)
+  
+  
+  # Adjusted ratios
   n_hit <- n_hit + 0.5
   n_fa <- n_fa + 0.5
-  hit_rate <- n_hit/(n_hit + n_miss + 1)
-  fa_rate <- n_fa/(n_fa + n_cr + 1)
+  hit_rate_adjusted <- n_hit/(n_hit + n_miss + 1)
+  fa_rate_adjusted <- n_fa/(n_fa + n_cr + 1)
 
+  
   # dprime
-  dprime <- qnorm(hit_rate) - qnorm(fa_rate)
+  dprime <- qnorm(hit_rate_adjusted) - qnorm(fa_rate_adjusted)
 
   # beta
-  zhr <- qnorm(hit_rate)
-  zfar <- qnorm(fa_rate)
+  zhr <- qnorm(hit_rate_adjusted)
+  zfar <- qnorm(fa_rate_adjusted)
   beta <- exp(-zhr*zhr/2+zfar*zfar/2)
 
   # aprime
