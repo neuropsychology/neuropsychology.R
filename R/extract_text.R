19310185b3f64dc493edeb22d6f0ff8be61f9c16
DominiqueMakowski <dom.mak19@gmail.com>
2017-04-05 18:46:26 +0200

    extract_text: support html

--- R/extract_text.R
+++ R/extract_text.R
@@ -1,38 +1,61 @@
-extract_text <- function(files=".",
+extract_text <- function(sources=".",
+                          type="pdf",
                           word.length.min=4,
                           word.length.max=Inf,
                           freq.min=10,
                           freq.max=Inf){
 
-  
-  
-  if(grepl(".pdf", files)){
-    files <- c(files)
-  } else{
-    files <- list.files(path=files, pattern = "pdf$")
+  # Transform to list if not list
+  if(length(sources) == 1){
+    sources <- c(sources)
   }
-  
-  if (length(files) > 1){
-    text <- c()
-    for(file in files){
-      text <- c(text, pdftools::pdf_text(file))
+
+
+  # Read PDFs
+  if(type == "pdf"){
+    if(grepl(".pdf", sources)){
+      sources <- c(sources)
+    } else{
+      sources <- list.files(path=sources, pattern = "pdf$")
+    }
+
+    for(source in sources){
+        text <- c(text, pdftools::pdf_text(source))
     }
     text <- unlist(text)
-  } else{
-    text <- pdftools::pdf_text(files)
+    }
+
+  # Scrap HTML
+  if(type == "html"){
+    text <- data.frame()
+    for(source in sources){
+      page <- xml2::read_html(source)
+      page <- page %>%
+        rvest::html_nodes("p") %>%
+        rvest::html_text() %>%
+        unlist() %>%
+        strsplit(split = " ") %>%
+        unlist() %>%
+        as.data.frame() %>%
+        dplyr::transmute_("word" = ".")
+      text <- rbind(text, page)
+    }
+    text <- text$word
   }
-  
+
+
+  # Clean
   text_clean <- tm::termFreq(text, control = list(removePunctuation = TRUE,
                                     tolower = TRUE,
                                     removeNumbers = TRUE,
                                     stopwords = TRUE,
                                     wordLengths=c(word.length.min, word.length.max)))
-  
+
   data <- data.frame(word=rownames(text_clean), freq=rowSums(as.matrix(text_clean))) %>%
-    dplyr::arrange_("desc(freq)") %>% 
-    filter_(paste("freq > ", freq.min)) %>% 
+    dplyr::arrange_("desc(freq)") %>%
+    filter_(paste("freq > ", freq.min)) %>%
     filter_(paste("freq < ", freq.max))
-    
+
   return(data)
 }
 
